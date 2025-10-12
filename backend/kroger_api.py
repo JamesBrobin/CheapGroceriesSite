e3c4a34f8bc93b04d9b4510394a9a9eb3fcf2e5e
James Brobin <jamesbrobin1@gmail.com>
2025-10-12 14:39:23 -0600

    Connected to Kroger API.

---
+++ backend/kroger_api.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KROGER_CLIENT_ID = os.getenv("KROGER_CLIENT_ID")
+KROGER_CLIENT_SECRET = os.getenv("KROGER_CLIENT_SECRET")
+
+def get_access_token():
+    url = "https://api.kroger.com/v1/connect/oauth2/token"
+    data = {
+        "grant_type": "client_credentials",
+        "scope": "product.compact"
+    }
+    auth = (KROGER_CLIENT_ID, KROGER_CLIENT_SECRET)
+    response = requests.post(url, data=data, auth=auth)
+    response.raise_for_status()
+    token = response.json()["access_token"]
+    return token
+
+def search_products(query, token, limit=5):
+    url = f"https://api.kroger.com/v1/products?filter.term={query}&filter.limit={limit}"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+if __name__ == "__main__":
+    #print(get_access_token())
+    token = get_access_token()
+    products = search_products("milk", token)
+    print(products)
+
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
-# main.py
 from fastapi import FastAPI
 
-# Create the FastAPI instance (this is what uvicorn looks for)
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello, world!"}
